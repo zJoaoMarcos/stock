d3b444d2219b1e5c29ca56a8ba22680beb236aa4
João Matos <joaomarcosm.silva@outlook.com>
2022-06-21 12:13:56 -0300

    styling the homepage

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,20 +1,23 @@
 import React, { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { AuthLoginContext } from "../../Contexts/AuthLogin";
+import { Container, Title, Options } from "./Style";
 
 export default function Home() {
     const navigate = useNavigate();
     const { user, signOut  } = useContext(AuthLoginContext);
     let userLogado = JSON.parse(user)
     return (
-        <div>
-            <h2>Wellcome to HomePage: {userLogado.email}</h2>
+        <Container>
+            <Title>Estoque TI </Title>
+            <Options>
 
-            <button onClick={() => navigate('/register')}>Cadastrar Item</button>
-            <button onClick={() => navigate('/stock')}>Estoque</button>
-            <button onClick={() => navigate('/shop')}>Lista de Compras</button>
-            <button onClick={() => navigate('/movements')}>Movimentos</button>
-            <button onClick={() => signOut()}>Sair</button>
-        </div>
+                <button onClick={() => navigate('/register')}>Cadastrar Item</button>
+                <button onClick={() => navigate('/stock')}>Estoque</button>
+                <button onClick={() => navigate('/shop')}>Lista de Compras</button>
+                <button onClick={() => navigate('/movements')}>Movimentos</button>
+                <button onClick={() => signOut()}>Sair</button>
+            </Options>
+        </Container>
     );
 }
