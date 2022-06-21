afc9b3530ee02506f572ffbe78b0bf31187dac6f
João Marcos <joaomarcosm.silva@outlook.com>
2022-06-20 22:00:56 -0300

    finish page login

--- src/Pages/Login/Style.js
+++ src/Pages/Login/Style.js
@@ -3,15 +3,15 @@ import { Form as Unform } from '@unform/web';
 
 export const Container = styled.div`
     background-color: white;
-    padding: 150px 30px;
+    padding: 140px 30px;
     display: flex;
     align-items: center;
     flex-direction: column;
-    height: 100vw
 `;
 
 export const Title = styled.h1`
     color: #9c000d;
+    font-size: 1.9em;
 `;
 
 export const Form = styled(Unform)`
@@ -19,30 +19,62 @@ export const Form = styled(Unform)`
     display: flex;
     align-items: center;
     flex-direction: column;
-    justify-content: center;
+    justify-content: space-evenly;
     box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
     border-radius: 10px;
-    min-width: 450px;
-    min-height: 400px;
+    width: 33%;
+    height: 350px;
 
     h2 {
         color: #9c000d;
+        font-size: 1.6em;
     }
 
     Input {
         height: 30px;
-        width: 350px;
+        width: 80%;
         margin-bottom: 25px;
-        border-radius: 8px;
-        border-style: none;
+        font-size: 1em;
+        border: none;
+        border-bottom: 2px solid #9c000d;
+        @media(max-width: 768px) {
+            width: 80%;
+        }
+    }
+
+    Input:focus {
+        outline: none;
+        border-bottom: 2px solid #9c000d;
+    }
+
+    Input::placeholder {
+        font-size: 1em;
     }
 
     button {
-        height: 35px;
-        width: 350px;
-        border-radius: 8px;
+        height: 40px;
+        width: 80%;
+        border-radius: 10px;
         border-style: none;
-        background-color:#9c000d;
+        background-color: #9c000d;
         color: white;
+        font-size: 1.2em;
+        @media(max-width: 768px) {
+            width: 80%;
+        }
+    }
+
+    button:hover {
+        background-color: #7d000a;
+    }
+
+    button:active {
+        font-weight: 700;
+        background-color: #9c000d;
+        
+    } 
+
+    @media(max-width: 768px) {
+        width: 100%;
     }
 `;
